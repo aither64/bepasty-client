77a6da2f76d45148aca4872a4cae1c54943b9416
Jakub Skokan <jakub.skokan@havefun.cz>
2023-05-14 09:31:20 +0200

    shell.nix: set MANPATH

--- shell.nix
+++ shell.nix
@@ -14,6 +14,7 @@ in stdenv.mkDerivation rec {
   shellHook = ''
     export GEM_HOME=$(pwd)/.gems
     export PATH="$GEM_HOME/bin:$PATH"
+    export MANPATH="$(pwd)/man:$(man --path)"
     gem install bundler
     bundle install
   '';
